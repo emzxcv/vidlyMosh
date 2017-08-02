d34dff8bf0b61a2ce7f678b466293ba0cca6f957
emxqm <emily.ha@globalx.com.au>
2017-08-02 16:44:49 +1000

    Add project files.

---
+++ VidlyMosh/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VidlyMosh.Startup))]
+namespace VidlyMosh
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
