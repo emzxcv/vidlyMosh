daae290bab7468205c352df6664343da1e524afa
emzxcv <emilyha17@gmail.com>
2017-08-15 19:08:04 +1000

    add movies controller

--- VidlyMosh/App_Start/RouteConfig.cs
+++ VidlyMosh/App_Start/RouteConfig.cs
@@ -13,6 +13,9 @@ namespace VidlyMosh
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.MapMvcAttributeRoutes();
+
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
